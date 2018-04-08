f36f26c552cba1797fb362e8ee1cf716e8f767e4
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2018-04-08 20:14:05 +0200

    Use node scss with hot reloading on a non linux host

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,24 +1,29 @@
+const path = require('path')
+
 module.exports = {
   mode: 'development',
-  entry: './index.js',
-  // module: {
-  //   rules: [{
-  //     test: /\.scss$/,
-  //     use: [{
-  //       loader: 'style-loader'
-  //     }, {
-  //       loader: 'css-loader'
-  //     }, {
-  //       loader: 'sass-loader'
-  //     }]
-  //   }]
-  // },
+  entry: {
+    'main': './index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
   plugins: [
     new require('copy-webpack-plugin')([
       { from: './index.html' }
     ])
   ],
   watchOptions: {
-    poll: true
+    aggregateTimeout: 300,
+    poll: 1000
   }
 }
